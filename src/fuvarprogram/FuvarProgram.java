e536a7cca3f4d2ae3b59d261c652848b62a6aca0
varadizsolt <varadizsolt0212@gmail.com>
2021-02-10 11:47:07 +0100

    2.feladat

--- src/fuvarprogram/FuvarProgram.java
+++ src/fuvarprogram/FuvarProgram.java
@@ -1,12 +1,17 @@
 
 package fuvarprogram;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 
 public class FuvarProgram {
 
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        List<String> sorok = Files.readAllLines(Paths.get("fuvar.csv"));
     }
     
 }
