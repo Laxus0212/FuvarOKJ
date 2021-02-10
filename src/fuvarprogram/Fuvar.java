0b40fb465176949e1aa202613bd8a4a45d54acf6
varadizsolt <varadizsolt0212@gmail.com>
2021-02-10 12:16:54 +0100

    3.feladat

--- src/fuvarprogram/Fuvar.java
+++ src/fuvarprogram/Fuvar.java
@@ -1,7 +1,66 @@
 
 package fuvarprogram;
 
+import java.text.ParseException;
+
 
 public class Fuvar {
+    //taxi_id;indulas;idotartam;tavolsag;viteldij;borravalo;fizetes_modja
     
+    private int id;
+    private String indulas;
+    private int idotartam;
+    private double tav;
+    private double dij;
+    private double borra;
+    private String fizMod;
+
+    public Fuvar(String sor) throws ParseException {
+        String [] s = sor.split(";");
+        this.id = Integer.parseInt(s[0]);
+        this.indulas = s[1];
+        this.idotartam = Integer.parseInt(s[2]);
+        
+        
+        s[3] = s[3].replace(",", ".");
+        this.tav = Double.parseDouble(s[3]);
+        s[4] = s[4].replace(",", ".");
+        this.dij = Double.parseDouble(s[4]);
+        s[5] = s[5].replace(",", ".");
+        this.borra = Double.parseDouble(s[5]);
+        this.fizMod = s[6];
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIndulas() {
+        return indulas;
+    }
+
+    public int getIdotartam() {
+        return idotartam;
+    }
+
+    public double getTav() {
+        return tav;
+    }
+
+    public double getDij() {
+        return dij;
+    }
+
+    public double getBorra() {
+        return borra;
+    }
+
+    public String getFizMod() {
+        return fizMod;
+    }
+
+    @Override
+    public String toString() {
+        return "\tTaxi azonosító: " + id + "\n Fuvar hossza: " + idotartam + " másodperc" + "\n Megtett távolság: " + tav + "\n Viteldíj: " + dij ;
+    }
 }
--- src/fuvarprogram/FuvarProgram.java
+++ src/fuvarprogram/FuvarProgram.java
@@ -4,14 +4,51 @@ package fuvarprogram;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class FuvarProgram {
 
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
+        //2.feladat
+        
         List<String> sorok = Files.readAllLines(Paths.get("fuvar.csv"));
+        ArrayList<Fuvar> fuvarok = new ArrayList<>();
+        for (int i = 1; i < sorok.size(); i++) {
+            String sor = sorok.get(i);
+            fuvarok.add(new Fuvar(sor));
+        }
+        //3.feladat
+        hanyadikFeladat(3);
+        System.out.println(fuvarok.size() + " fuvar");
+//        //4.feladat
+//        hanyadikFeladat(4);
+//        int db = 0;
+//        double bevetel = 0.0;
+//        for (Fuvar fuvar : fuvarok) {
+//            if (fuvar.getId()==6185) {
+//                db++;
+//                bevetel += fuvar.getDij() + fuvar.getBorra();
+//            }
+//        }
+//        System.out.println(db + " fuvar alatt: " + bevetel + "$");
+//        //5.feladat
+//        hanyadikFeladat(5);
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    public static void hanyadikFeladat(int hanyadik){
+        System.out.print(hanyadik + ". feladat: ");
     }
     
 }
